54c0db5a312873704b2f2a495a9cf66d043ab10a
Jae-Yong Yoo <yjaeyong@gmail.com>
2012-04-09 19:46:48 +0900

    remove unnecessary files

--- elements/local/VxS/dxtc.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DXTC_H__
-#define __DXTC_H__
-
-extern "C" void CompressDXT(unsigned char *out, uint* block_image, uint *d_data, uint *d_result, uint blocks, uint blocksPerLaunch, uint compressedSize);
-
-#endif
